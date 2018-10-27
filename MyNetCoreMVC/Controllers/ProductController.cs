08f57078aff88f4ea92ed95a5e56001d94df429e
Tinh77 <tinhgiang9898@gmail.com>
2018-10-27 13:15:18 +0700

    push

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreMVC.Models;
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetList()
+        {
+            
+            return View();
+        }
+        
+        public IActionResult Create(string name, string price)
+        {
+            ViewData["name"] = name;
+            ViewData["price"] = price;
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        [Route("Product/Delete/{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return new JsonResult(new Product() {
+                id = id
+            });
+        }
+    }
+}
